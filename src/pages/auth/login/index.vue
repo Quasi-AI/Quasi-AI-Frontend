6d5291c95c6cdae63475eadc325cb4ac7faaf7a5
danieladeabah <daniel.adeabah@amalitech.com>
2025-02-22 14:59:19 +0000

    Adjust layout spacing and update Google sign-in button text for clarity

--- src/pages/auth/login/index.vue
+++ src/pages/auth/login/index.vue
@@ -12,7 +12,7 @@
         <h1 class="text-2xl font-bold">QUASI AI</h1>
       </NuxtLink>
 
-      <div class="mx-12 flex flex-col items-center justify-center space-y-2">
+      <div class="mx-3 flex flex-col items-center justify-center space-y-2">
         <v-alert v-if="errorMessage" type="error" class="w-full">{{
           errorMessage
         }}</v-alert>
@@ -31,7 +31,7 @@
               d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
             />
           </svg>
-          <span>Sign in with Google As Student / Educator</span>
+          <span>Sign in with Google As Student or Educator</span>
         </UButton>
       </div>
 
