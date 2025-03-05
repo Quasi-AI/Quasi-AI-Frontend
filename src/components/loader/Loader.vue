247b5687311bdd14e5fb41f0e8b15656a5b357fb
Rexton Itsiah <71719147+RexSOFT-ware12@users.noreply.github.com>
2025-03-05 02:33:27 +0000

    implemented login sign up

--- src/components/loader/Loader.vue
+++ src/components/loader/Loader.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="h-5 w-5 animate-spin rounded-full border-t-2 border-white"></div>
+  <div class="relative flex items-center justify-center">
+    <div class="h-10 w-10 animate-spin rounded-full border-4 border-transparent border-t-blue-500 border-r-blue-500"></div>
+    <div class="absolute h-10 w-10 rounded-full border-4 border-blue-500 opacity-30 animate-pulse"></div>
+    <div class="absolute h-6 w-6 rounded-full bg-blue-500 opacity-50 animate-ping"></div>
+  </div>
 </template>
 
 <script setup></script>
