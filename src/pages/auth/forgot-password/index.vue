a6ce510f1377e9e3d4483a1d8ebca66c8ce199ae
Rexton Itsiah <71719147+RexSOFT-ware12@users.noreply.github.com>
2025-03-05 04:33:39 +0000

    added password reset, password change

--- src/pages/auth/forgot-password/index.vue
+++ src/pages/auth/forgot-password/index.vue
@@ -1,47 +1,58 @@
 <template>
-  <NuxtLayout name="auth-wrapper">
-    <div
-      class="w-full max-w-[569px] rounded-2xl bg-white py-6 text-center text-2xl lg:shadow-md"
-    >
-      <NuxtLink to="/" class="flex items-center justify-center gap-2">
+  <div class="flex h-screen">
+    <div class="w-full md:w-1/2 h-full flex flex-col justify-center items-center bg-white px-10">
+      <NuxtLink to="/" class="mb-4 flex items-center justify-center gap-2">
         <img
           src="https://raw.githubusercontent.com/Quasi-AI/.github/refs/heads/main/quasiailogo.png"
-          alt="logo"
-          class="w-10 cursor-pointer"
+          alt="QUASI AI Logo"
+          class="h-10 cursor-pointer"
         />
         <h1 class="text-2xl font-bold">QUASI AI</h1>
       </NuxtLink>
+      <p class="text-gray-500 mb-6">We will send you reset instruction in your email</p>
 
-      <div class="mx-8 flex flex-col gap-4 py-6 sm:mx-12">
-        <UInput
-          v-model="email"
-          type="email"
-          placeholder="Enter email"
-          maxLength="250"
-          size="xl"
-          variant="none"
-          class="w-full rounded-2xl bg-[#F1F3FE]"
-        />
-
-        <UButton
-          variant=""
-          class="flex w-full items-center justify-center rounded-md bg-[#5D3BEA] text-white"
-          label="Sign in"
-          size="md"
-          :disabled="!isValidEmail(email) || isLoading"
-          @click="forgotPassword"
-        >
-          <span v-if="!isLoading">Recover Account</span>
-          <span v-else class="flex items-center">
-            <Loader class="h-5 w-5" />
-          </span>
+      <form class="w-full max-w-sm">
+        <!-- Email Field -->
+        <div class="relative w-full mb-4">
+          <input
+            v-model="email"
+            type="email"
+            id="email"
+            class="peer w-full p-3 border border-gray-300 rounded focus:border-purple-500 focus:ring-0 placeholder-transparent"
+            placeholder="Email"
+          />
+          <label for="email"
+            class="absolute left-3 top-1/2 -translate-y-1/2 bg-white px-1 text-gray-500 text-sm transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:top-0 peer-focus:text-xs peer-focus:text-purple-600"
+          >
+            Email
+          </label>
+        </div>
+        <UButton 
+            class="w-full bg-[#5D3BEA] items-center justify-center text-white p-3 rounded mb-4" 
+            size="md"
+            :disabled="isLoading || !isValidForm"
+            @click="forgotPassword"
+            variant="none">
+            <span v-if="!isLoading">Send reset instructions</span>
+            <span v-else class="flex items-center">
+              <Loader class="h-5 w-5 animate-spin" />
+            </span>
         </UButton>
-        <NuxtLink to="/auth/login" class="text-sm text-[#5D3BEA]"
-          >Back to login</NuxtLink
-        >
-      </div>
+      </form>
+
+      <p class="text-sm text-black">
+        Remember password?
+        <NuxtLink to="/auth/login" class="text-[#5D3BEA]">Login</NuxtLink>
+      </p>
     </div>
-  </NuxtLayout>
+    
+    <div class="w-1/2 bg-gray-100 rounded-2xl overflow-hidden m-4 md:block">
+      <img 
+        src="https://firebasestorage.googleapis.com/v0/b/park4me-b2127.appspot.com/o/freepik__the-style-is-candid-image-photography-with-natural__28525.png?alt=media&token=8a662acd-7725-41cb-9601-785985db76b9" 
+        class="h-full w-full object-cover rounded-2xl"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -74,6 +85,7 @@ const forgotPassword = async () => {
     }
   }
 
+  
   isLoading.value = false
 
   setTimeout(() => {
@@ -81,4 +93,9 @@ const forgotPassword = async () => {
     successMessage.value = null
   }, 3000)
 }
+
+
+const isValidForm = computed(() => {
+  return email.value && isValidEmail(email.value)
+})
 </script>
--- src/pages/auth/password-reset/[index].vue
+++ src/pages/auth/password-reset/[index].vue
@@ -1,78 +1,95 @@
+
 <template>
-  <NuxtLayout name="auth-wrapper">
-    <div
-      class="w-full max-w-[569px] rounded-2xl bg-white py-6 text-center text-2xl lg:shadow-md"
-    >
-      <NuxtLink to="/" class="flex items-center justify-center gap-2">
+  <div class="flex h-screen">
+    <div class="w-full md:w-1/2 h-full flex flex-col justify-center items-center bg-white px-10">
+      <NuxtLink to="/" class="mb-4 flex items-center justify-center gap-2">
         <img
           src="https://raw.githubusercontent.com/Quasi-AI/.github/refs/heads/main/quasiailogo.png"
-          alt="logo"
-          class="w-10 cursor-pointer"
+          alt="QUASI AI Logo"
+          class="h-10 cursor-pointer"
         />
         <h1 class="text-2xl font-bold">QUASI AI</h1>
       </NuxtLink>
+      <p class="text-gray-500 mb-6">We will send you reset instruction in your email</p>
 
-      <div class="mx-8 flex flex-col gap-4 py-6 sm:mx-12">
-        <div class="relative">
-          <UInput
-            :type="isPasswordVisible ? 'text' : 'password'"
-            placeholder="Password"
-            v-model="confirm_password"
-            maxLength="250"
-            size="xl"
-            variant="none"
-            class="w-full rounded-2xl bg-[#F1F3FE]"
-          />
-          <span
-            class="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3"
-            @click="togglePasswordVisibility"
-          >
-            <UIcon
-              :name="
-                isPasswordVisible ? 'i-heroicons-eye' : 'i-heroicons-eye-slash'
-              "
-            ></UIcon>
-          </span>
-        </div>
-
-        <div class="relative">
-          <UInput
-            :type="isPasswordVisible ? 'text' : 'password'"
-            placeholder="Confirm Password"
-            v-model="password"
-            maxLength="250"
-            size="xl"
-            variant="none"
-            class="w-full rounded-2xl bg-[#F1F3FE]"
-          />
-          <span
-            class="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3"
-            @click="togglePasswordVisibility"
-          >
-            <UIcon
-              :name="
-                isPasswordVisible ? 'i-heroicons-eye' : 'i-heroicons-eye-slash'
-              "
-            ></UIcon>
-          </span>
-        </div>
-
-        <UButton
-          variant=""
-          class="flex w-full items-center justify-center rounded-md bg-[#5D3BEA] text-white"
-          label="Sign in"
-          size="md"
-          @click="newPassword"
-        >
-          <span v-if="!isLoading">Change password</span>
-          <span v-else class="flex items-center">
-            <Loader class="h-5 w-5" />
-          </span>
+      <form class="w-full max-w-sm">
+          <div class="relative w-full mb-4">
+            <input
+              :type="isPasswordVisible ? 'text' : 'password'"
+              v-model="confirm_password"
+              id="password"
+              class="peer w-full p-3 border border-gray-300 rounded focus:border-purple-500 focus:ring-0 placeholder-transparent"
+              placeholder="Password"
+            />
+            <label for="password"
+              class="absolute left-3 top-1/2 -translate-y-1/2  px-1 text-gray-500 text-sm transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:top-0 peer-focus:text-xs peer-focus:text-purple-600 bg-white"
+            >
+              Password
+            </label>
+            <span
+              class="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+              @click="togglePasswordVisibility"
+            >
+              <UIcon
+                :name="
+                  isPasswordVisible ? 'i-heroicons-eye' : 'i-heroicons-eye-slash'
+                "
+                class="text-gray-500"
+              ></UIcon>
+            </span>
+          </div>
+          <div class="relative w-full mb-4">
+            <input
+              :type="isPasswordVisible ? 'text' : 'password'"
+              v-model="password"
+              id="password"
+              class="peer w-full p-3 border border-gray-300 rounded focus:border-purple-500 focus:ring-0 placeholder-transparent"
+              placeholder="Password"
+            />
+            <label for="password"
+              class="absolute left-3 top-1/2 -translate-y-1/2  px-1 text-gray-500 text-sm transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:top-0 peer-focus:text-xs peer-focus:text-purple-600 bg-white"
+            >
+              Password
+            </label>
+            <span
+              class="absolute inset-y-0 right-3 flex items-center cursor-pointer"
+              @click="togglePasswordVisibility"
+            >
+              <UIcon
+                :name="
+                  isPasswordVisible ? 'i-heroicons-eye' : 'i-heroicons-eye-slash'
+                "
+                class="text-gray-500"
+              ></UIcon>
+            </span>
+          </div>
+        <UButton 
+            class="w-full bg-[#5D3BEA] items-center justify-center text-white p-3 rounded mb-4" 
+            size="md"
+            @click="newPassword"
+            variant="none">
+            <span v-if="!isLoading">Save new password</span>
+            <span v-else class="flex items-center">
+              <Loader class="h-5 w-5 animate-spin" />
+            </span>
         </UButton>
-      </div>
+      </form>
+
+      <p class="text-sm text-black">
+        Didn't Redirect?
+        <NuxtLink to="/auth/login" class="text-[#5D3BEA]">Login</NuxtLink>
+      </p>
     </div>
-  </NuxtLayout>
+    
+    <div class="w-1/2 bg-gray-100 rounded-2xl overflow-hidden m-4 md:block">
+      <img 
+        src="https://firebasestorage.googleapis.com/v0/b/park4me-b2127.appspot.com/o/freepik__the-style-is-candid-image-photography-with-natural__28525.png?alt=media&token=8a662acd-7725-41cb-9601-785985db76b9" 
+        class="h-full w-full object-cover rounded-2xl"
+      />
+    </div>
+  </div>
 </template>
+
 
 <script setup>
 import Loader from '@/components/loader/Loader.vue'
