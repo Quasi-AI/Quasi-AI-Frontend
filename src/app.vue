57c99f5c4f6bfb171daa9d209d3cdbdf3d568dd5
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-05 19:16:31 +0000

    Remove unused notification watcher from app component for improved clarity

--- src/app.vue
+++ src/app.vue
@@ -2,9 +2,3 @@
   <NuxtPage />
   <UNotifications />
 </template>
-
-<script setup lang="ts">
-import { useNotificationWatcher } from '@/composables/useNotificationWatcher'
-
-useNotificationWatcher()
-</script>
