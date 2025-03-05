1ed87d72ed313259e08ef24af6422276c84669a1
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-05 21:37:59 +0000

    Refactor Nuxt configuration for improved readability and maintainability

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 export default defineNuxtConfig({
   nitro: {
     prerender: {
-      crawlLinks: false, // Disable automatic crawling
-      routes: [] // Remove predefined routes
+      crawlLinks: false,
+      routes: []
     },
     routeRules: {
       '/**': {
@@ -16,29 +16,21 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['@/assets/css/main.css'],
   modules: ['nuxt-icon', '@nuxt/ui', '@nuxtjs/color-mode'],
-
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {}
     }
   },
-
   colorMode: {
     preference: 'light'
   },
-
   runtimeConfig: {
     public: {
       GOOGLE_GEMINI_API_KEY: process.env.GOOGLE_GEMINI_API_KEY
     }
   },
-
   compatibilityDate: '2025-02-10',
-
-  // Add plugins here
   plugins: ['~/plugins/fontawesome.js'],
-
-  // avoid server side rendering
   ssr: false
 })
