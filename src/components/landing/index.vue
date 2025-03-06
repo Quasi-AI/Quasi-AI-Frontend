4d8c266f31556514ce9707ba877bbc1b844f2d57
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-06 23:33:14 +0000

    Increase top margin of content section for improved spacing

--- src/components/landing/index.vue
+++ src/components/landing/index.vue
@@ -75,7 +75,7 @@
         width="100%"
         class="animate-fade-in h-full cursor-pointer lg:absolute lg:top-[-50px] lg:h-[230px]"
       />
-      <div class="mt-4 bg-white px-4 lg:mt-[200px] lg:px-0">
+      <div class="mt-12 bg-white px-4 lg:mt-[200px] lg:px-0">
         <h2
           class="animate-slide-up text-center text-3xl font-bold text-[#5D3BEA] lg:text-4xl"
         >
