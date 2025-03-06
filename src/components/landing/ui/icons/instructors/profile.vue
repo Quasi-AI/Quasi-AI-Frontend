761427ae1f7e3a1632fb62eaf2b1063844da8955
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-06 04:44:24 +0000

    Add Profile icon component for landing page

---
+++ src/components/landing/ui/icons/instructors/profile.vue
@@ -0,0 +1,44 @@
+<template>
+  <svg
+    width="80"
+    height="80"
+    viewBox="0 0 80 80"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="80" height="80" fill="#564FFD" fill-opacity="0.1" />
+    <path
+      opacity="0.2"
+      d="M53.75 27.5H26.25C25.9185 27.5 25.6005 27.6317 25.3661 27.8661C25.1317 28.1005 25 28.4185 25 28.75V53.75L30 51.25L35 53.75L40 51.25L45 53.75L50 51.25L55 53.75V28.75C55 28.4185 54.8683 28.1005 54.6339 27.8661C54.3995 27.6317 54.0815 27.5 53.75 27.5ZM37.5 45H30V35H37.5V45Z"
+      fill="#564FFD"
+    />
+    <path
+      d="M25 53.75V28.75C25 28.4185 25.1317 28.1005 25.3661 27.8661C25.6005 27.6317 25.9185 27.5 26.25 27.5H53.75C54.0815 27.5 54.3995 27.6317 54.6339 27.8661C54.8683 28.1005 55 28.4185 55 28.75V53.75L50 51.25L45 53.75L40 51.25L35 53.75L30 51.25L25 53.75Z"
+      stroke="#564FFD"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M42.5 37.5H50"
+      stroke="#564FFD"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M42.5 42.5H50"
+      stroke="#564FFD"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M37.5 35H30V45H37.5V35Z"
+      stroke="#564FFD"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
