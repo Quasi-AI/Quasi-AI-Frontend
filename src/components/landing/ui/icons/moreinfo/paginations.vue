4fe2f3c1a8bb758f40686b348d646ea5e114b1b4
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-06 04:45:14 +0000

    Add pagination icon component for landing page

---
+++ src/components/landing/ui/icons/moreinfo/paginations.vue
@@ -0,0 +1,89 @@
+<template>
+  <svg
+    width="128"
+    height="88"
+    viewBox="0 0 128 88"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect
+      width="48"
+      height="48"
+      transform="matrix(-1 0 0 1 48 14)"
+      fill="#F5F7FA"
+    />
+    <path
+      d="M32.25 38H15.75"
+      stroke="#1D2026"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M22.5 31.25L15.75 38L22.5 44.75"
+      stroke="#1D2026"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <g filter="url(#filter0_d_72_1399)">
+      <rect
+        width="48"
+        height="48"
+        transform="translate(60 14)"
+        fill="#FF6636"
+      />
+      <path
+        d="M75.75 38H92.25"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M85.5 31.25L92.25 38L85.5 44.75"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_72_1399"
+        x="40"
+        y="0"
+        width="88"
+        height="88"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="6" />
+        <feGaussianBlur stdDeviation="10" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0.8 0 0 0 0 0.321569 0 0 0 0 0.168627 0 0 0 0.5 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_72_1399"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_72_1399"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+</template>
