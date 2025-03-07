92b524495855baa596d1e36800bc1b978ab826ce
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-07 11:01:06 +0000

    Add games page layout using CommonSidebar and CommonNavbar for consistency

---
+++ src/pages/other/games.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout name="app-wrapper">
+    <template #sidebar>
+      <CommonSidebar />
+    </template>
+    <template #apps>
+      <CommonNavbar />
+      <AppsGames />
+    </template>
+  </NuxtLayout>
+</template>
