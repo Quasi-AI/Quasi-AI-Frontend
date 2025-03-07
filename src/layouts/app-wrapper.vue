8f6f7ee8d1d9f349698a91148956e2f917b39d88
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-07 21:56:51 +0000

    Remove commented-out sidebar and main content sections for cleaner code

--- src/layouts/app-wrapper.vue
+++ src/layouts/app-wrapper.vue
@@ -2,14 +2,12 @@
   <ClientOnly>
     <CommonNavbar />
     <div class="flex w-full bg-[#F4F7FE] dark:bg-[#0C1438]">
-      <!-- Sidebar (Hidden on Mobile) -->
       <div
         class="hidden min-h-screen w-64 bg-gray-200 transition-all duration-300 lg:block dark:bg-gray-900"
       >
         <slot name="sidebar"></slot>
       </div>
 
-      <!-- Main Content (Pushes Right on Larger Screens) -->
       <div class="flex-1">
         <div class="overflow-y-auto overflow-x-hidden">
           <slot name="apps"></slot>
