1972d88c4382ff4a17cd412ad094b5e37daa7c89
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-07 11:00:55 +0000

    Add support page layout with CommonSidebar and CommonNavbar components

---
+++ src/pages/apps/support.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout name="app-wrapper">
+    <template #sidebar>
+      <CommonSidebar />
+    </template>
+    <template #apps>
+      <CommonNavbar />
+      <AppsOtherSupport />
+    </template>
+  </NuxtLayout>
+</template>
