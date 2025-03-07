356cb6c60fcaf2aa6f59625ca900ec9a41ef2cfe
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-07 11:01:15 +0000

    Add plan page layout using CommonSidebar and CommonNavbar for consistency

---
+++ src/pages/other/plan.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout name="app-wrapper">
+    <template #sidebar>
+      <CommonSidebar />
+    </template>
+    <template #apps>
+      <CommonNavbar />
+      <AppsOtherPlan />
+    </template>
+  </NuxtLayout>
+</template>
