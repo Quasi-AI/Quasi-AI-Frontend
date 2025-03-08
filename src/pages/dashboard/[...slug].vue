3bb8a54dc394cf6ba14cad99b968f4379aed1a3b
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-08 17:35:42 +0000

    Update analuytics UI to match darkmode (mock UI)

--- src/pages/dashboard/[...slug].vue
+++ src/pages/dashboard/[...slug].vue
@@ -6,9 +6,7 @@
     <template #apps>
       <CommonDashboardMockDashboard width="100%" height="100%" />
 
-      <div
-        class="animate-fade-in grid flex-1 grid-cols-1 gap-8 lg:grid-cols-3"
-      >
+      <div class="animate-fade-in grid flex-1 grid-cols-1 gap-8 lg:grid-cols-3">
         <div
           v-for="(feature, index) in features"
           :key="index"
