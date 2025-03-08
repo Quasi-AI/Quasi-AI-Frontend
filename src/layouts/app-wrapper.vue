2402d9a4e8385ae82f46aa14a6b33eb9249b8075
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-08 01:09:34 +0000

    Refactor app wrapper layout for improved structure and dynamic page titles

--- src/layouts/app-wrapper.vue
+++ src/layouts/app-wrapper.vue
@@ -10,10 +10,43 @@
 
       <div class="flex-1">
         <div class="overflow-y-auto overflow-x-hidden">
-          <slot name="apps"></slot>
-          <CommonNavbarBottom />
+          <div class="mx-auto flex max-w-6xl flex-col items-center p-4 lg:px-0">
+            <div class="flex w-full flex-col">
+              <h1
+                class="animate-slide-up my-4 pl-5 text-3xl font-medium dark:text-gray-400"
+              >
+                {{ pageTitle }}
+              </h1>
+
+              <slot name="apps"></slot>
+              <CommonNavbarBottom />
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </ClientOnly>
 </template>
+
+<script setup>
+const route = useRoute()
+
+// Define route-based titles
+const titles = {
+  '/dashboard': 'Dashboard',
+  '/apps': 'Browse top apps',
+  '/apps/questions': 'Questions',
+  '/apps/flashcards': 'Smart Flashcards',
+  '/apps/find-tutors': 'Tutors',
+  '/apps/essay': 'Essay Analyzer',
+  '/apps/quizzes': 'Quizzes',
+  '/apps/ai-tutor': 'AI Tutor',
+  '/apps/speech-to-text': 'Speech to Text Converter',
+  '/other/support': 'Customer Support',
+  '/other/games': 'Games',
+  '/other/past-questions': 'Past Questions'
+}
+
+// Compute the page title dynamically
+const pageTitle = computed(() => titles[route.path] || 'Page not found')
+</script>
