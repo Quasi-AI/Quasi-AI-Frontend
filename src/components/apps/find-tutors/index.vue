ddc0fc2ac238b799c4e47a368783caf6f5a8ea20
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-09 21:05:28 +0000

    Integrate find tutor

--- src/components/apps/find-tutors/index.vue
+++ src/components/apps/find-tutors/index.vue
@@ -13,7 +13,7 @@
 
       <button
         class="mr-4 rounded-lg bg-[#5D3BEA] px-6 py-1 text-white transition duration-300 hover:scale-105 hover:bg-[#4A2DCA]"
-        @click="createEditTutorModal = true"
+        @click="openCreateTutorModal"
       >
         Create Tutor
       </button>
@@ -24,21 +24,22 @@
       class="grid w-full grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
     >
       <div
-        v-for="tutor in filteredTutors"
+        v-for="(tutor, index) in filteredTutors"
         :key="tutor.id"
         class="animate-slide-up cursor-pointer rounded-lg bg-white transition-all duration-300 hover:shadow-xl dark:bg-[#111C44]"
         :style="{ animationDelay: `${index * 0.2}s` }"
+        :class="{ 'pointer-events-none opacity-50': tutor.status }"
       >
         <img
-          :src="tutor.image"
+          :src="tutor.image || defaultProfileImage"
           alt="Tutor"
           class="h-80 w-full rounded-t-lg object-cover lg:h-40"
           @click="openModal(tutor)"
         />
         <div class="p-2">
           <h2 class="mt-3 text-lg font-semibold">{{ tutor.name }}</h2>
-          <p class="text-gray-500">{{ tutor.subject }}</p>
-
+          <p class="text-gray-400">{{ tutor.subject }}</p>
+          <p v-if="tutor.status" class="py-2 text-red-400">Inactive</p>
           <div class="mt-6 flex items-center justify-between">
             <p
               @click="openModal(tutor)"
@@ -46,13 +47,17 @@
             >
               View biography
             </p>
-            <div class="flex items-center space-x-2">
+            <!-- Show Edit and Delete icons only if user_id matches -->
+            <div
+              v-if="tutor.user_id === localStorageUserId"
+              class="flex items-center space-x-2"
+            >
               <EditIcon
-                @click="createEditTutorModal = true"
+                @click="openEditTutorModal(tutor)"
                 class="h-4 w-4 cursor-pointer"
               />
               <DeleteIcon
-                @click="deleteTutorModal = true"
+                @click="openDeleteTutorModal(tutor)"
                 class="h-4 w-4 cursor-pointer"
               />
             </div>
@@ -82,13 +87,10 @@
         <!-- Profile Image -->
         <div class="mt-10 flex flex-col items-center">
           <img
-            :src="selectedTutor.image"
+            :src="selectedTutor.image || defaultProfileImage"
             alt="Tutor"
             class="h-80 rounded-t-lg transition hover:opacity-80"
           />
-          <p  @click="createEditTutorModal = true" class="mt-2 cursor-pointer text-sm text-gray-400 underline">
-            Click to change picture
-          </p>
         </div>
 
         <!-- Tutor Info -->
@@ -96,7 +98,7 @@
           <h2 class="text-2xl font-semibold text-gray-900 dark:text-white">
             {{ selectedTutor.name }}
           </h2>
-          <p class="mt-1 text-gray-500">Subject: {{ selectedTutor.subject }}</p>
+          <p class="mt-1 text-gray-500">{{ selectedTutor.subject }}</p>
         </div>
 
         <!-- Biography -->
@@ -107,7 +109,16 @@
           <p
             class="mt-2 max-h-40 overflow-auto text-sm text-gray-600 dark:text-gray-400"
           >
-            {{ selectedTutor.bio }}
+            {{ selectedTutor.bios }}
+          </p>
+
+          <h3 class="mt-5 text-lg font-medium text-gray-800 dark:text-gray-300">
+            Price
+          </h3>
+          <p
+            class="mt-2 max-h-40 overflow-auto text-sm text-gray-600 dark:text-gray-400"
+          >
+            {{ selectedTutor.price }}
           </p>
         </div>
 
@@ -173,9 +184,9 @@
         </template>
         <!-- Profile Picture Upload -->
         <div class="flex flex-col items-center">
-          <label for="file-upload" class="relative cursor-pointer">
+          <label for="file-upload" class="relative cursor-pointer text-left">
             <img
-              :src="profileImage || defaultProfileImage"
+              :src="profileImageSrc || defaultProfileImage"
               class="h-24 w-24 rounded-full border-2 border-gray-300 object-cover"
               alt="Profile"
             />
@@ -183,15 +194,12 @@
               id="file-upload"
               type="file"
               class="hidden"
-              @change="uploadImage"
+              @change="handleFileChange"
             />
+            <p class="mt-2 cursor-pointer text-sm text-blue-600">
+              Upload picture
+            </p>
           </label>
-          <p
-            class="mt-2 cursor-pointer text-sm text-blue-600"
-            @click="triggerFileUpload"
-          >
-            Click to upload picture
-          </p>
         </div>
 
         <!-- Form Fields -->
@@ -204,9 +212,9 @@
             class="my-2 w-full rounded-lg border bg-white p-2 dark:border-none dark:bg-[#111C44]"
           />
           <UInput
-            v-model="tutor.subjects"
-            label="Subjects"
-            placeholder="Enter subjects"
+            v-model="tutor.subject"
+            label="Subject"
+            placeholder="Enter subject"
             variant="none"
             class="my-2 w-full rounded-lg border bg-white p-2 dark:border-none dark:bg-[#111C44]"
           />
@@ -219,7 +227,7 @@
             class="my-2 w-full rounded-lg border bg-white p-2 dark:border-none dark:bg-[#111C44]"
           />
           <UTextarea
-            v-model="tutor.biography"
+            v-model="tutor.bios"
             label="Biography"
             placeholder="Enter biography"
             variant="none"
@@ -228,19 +236,21 @@
         </div>
 
         <!-- Modal Actions -->
-        <div class="mt-6 flex justify-end space-x-2">
+        <div
+          class="mt-6 flex flex-col items-center gap-3 lg:flex-row lg:justify-end"
+        >
           <UButton
             color="gray"
             @click="closeCreateEditModal"
             variant="none"
-            class="flex w-[130px] items-center justify-center rounded-md border border-[#5D3BEA] bg-white py-2 text-[#5D3BEA] transition hover:scale-105 hover:bg-gray-300"
+            class="flex w-[150px] items-center justify-center rounded-md border border-[#5D3BEA] bg-white py-2 text-[#5D3BEA] transition hover:scale-105 hover:bg-gray-300 lg:w-[150px]"
             >Close modal</UButton
           >
           <UButton
             @click="saveTutor"
             class="flex w-[150px] items-center justify-center rounded-md bg-[#5D3BEA] py-2 text-white transition hover:scale-105 hover:bg-[#4A2DCA]"
             variant="none"
-            >Add tutor</UButton
+            >{{ isEditing ? 'Update Tutor' : 'Add Tutor' }}</UButton
           >
         </div>
       </UCard>
@@ -279,10 +289,16 @@
 </template>
 
 <script setup>
+import axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import EditIcon from '~/assets/icons/edit-icon.vue'
 import DeleteIcon from '~/assets/icons/delete-icon.vue'
-import { tutors } from '~/constants/tutors'
+import {
+  storage,
+  storageRef,
+  uploadBytes,
+  getDownloadURL
+} from '~/utils/firebase'
 
 const createEditTutorModal = ref(false)
 const deleteTutorModal = ref(false)
@@ -291,69 +307,179 @@ const selectedTutor = ref({})
 const searchQuery = ref('')
 const chatMessages = ref([])
 const newMessage = ref('')
+const isEditing = ref(false)
+const tutors = ref([])
+const localStorageUserId = ref(localStorage.getItem('user_id'))
+
+const tutor = ref({
+  id: null,
+  name: '',
+  subject: '',
+  price: '',
+  bios: '',
+  image: ''
+})
+
+const profileImageSrc = ref('') // For image preview
+const fileInput = ref(null) // Reference to file input
+const defaultProfileImage =
+  'https://cdn-icons-png.flaticon.com/512/929/929422.png'
+
+// Fetch all tutors on component mount
+onMounted(async () => {
+  await fetchTutors()
+})
+
+const fetchTutors = async () => {
+  try {
+    const response = await axios.get(
+      'https://dark-caldron-448714-u5.uc.r.appspot.com/tutor/all'
+    )
+    tutors.value = response.data
+  } catch (error) {
+    console.error('Error fetching tutors:', error)
+  }
+}
 
 const filteredTutors = computed(() => {
-  return tutors.value.filter(tutor =>
-    tutor.subject.toLowerCase().includes(searchQuery.value.toLowerCase())
+  return tutors.value.filter(
+    tutor =>
+      tutor.name.toLowerCase().includes(searchQuery.value.toLowerCase()) ||
+      tutor.subject.toLowerCase().includes(searchQuery.value.toLowerCase())
   )
 })
 
 const openModal = tutor => {
-  selectedTutor.value = tutor
-  showModal.value = true
+  if (!tutor.status) {
+    selectedTutor.value = tutor
+    showModal.value = true
+  }
 }
 
 const closeModal = () => {
   showModal.value = false
 }
 
-const sendMessage = () => {
-  if (newMessage.value.trim() !== '') {
-    chatMessages.value.push({ sender: 'user', text: newMessage.value })
-    // Simulate tutor response
-    setTimeout(() => {
-      chatMessages.value.push({
-        sender: 'tutor',
-        text: 'Thank you for reaching out!'
-      })
-    }, 1000)
-    newMessage.value = ''
+const openCreateTutorModal = () => {
+  isEditing.value = false
+  tutor.value = {
+    id: null,
+    name: '',
+    subject: '',
+    price: '',
+    bios: '',
+    image: ''
+  }
+  profileImageSrc.value = ''
+  createEditTutorModal.value = true
+}
+
+const openEditTutorModal = tutorData => {
+  isEditing.value = true
+  tutor.value = { ...tutorData }
+  profileImageSrc.value = tutorData.image
+  createEditTutorModal.value = true
+}
+
+const openDeleteTutorModal = tutorData => {
+  selectedTutor.value = tutorData
+  deleteTutorModal.value = true
+}
+
+const handleFileChange = async event => {
+  const file = event.target.files[0]
+  if (file) {
+    // Display preview
+    profileImageSrc.value = URL.createObjectURL(file)
+
+    // Upload to Firebase
+    try {
+      const filePath = `tutors/${Date.now()}_${file.name}`
+      const storageReference = storageRef(storage, filePath)
+
+      // Upload file to Firebase
+      const snapshot = await uploadBytes(storageReference, file)
+
+      // Get the public URL
+      const downloadURL = await getDownloadURL(snapshot.ref)
+
+      // Save the URL to the tutor object
+      tutor.value.image = downloadURL
+    } catch (error) {
+      console.error('Error uploading to Firebase:', error)
+    }
   }
 }
 
-const tutor = ref({
-  name: '',
-  subjects: '',
-  price: '',
-  biography: ''
-})
-const profileImage = ref(null)
-const defaultProfileImage =
-  'https://cdn-icons-png.flaticon.com/512/929/929422.png'
+const saveTutor = async () => {
+  try {
+    const user_id = localStorage.getItem('user_id')
+    if (!user_id) {
+      console.error('User ID not found in localStorage')
+      return
+    }
 
-const triggerFileUpload = () => document.getElementById('file-upload').click()
+    // Prepare the payload
+    const payload = {
+      name: tutor.value.name,
+      subject: tutor.value.subject,
+      price: tutor.value.price,
+      bios: tutor.value.bios,
+      user_id: user_id,
+      image: tutor.value.image // Firebase image URL
+    }
 
-const uploadImage = event => {
-  const file = event.target.files[0]
-  if (file) profileImage.value = URL.createObjectURL(file)
+    if (isEditing.value) {
+      await axios.put(
+        `https://dark-caldron-448714-u5.uc.r.appspot.com/edit-tutor/${tutor.value._id}`,
+        payload
+      )
+    } else {
+      await axios.post(
+        'https://dark-caldron-448714-u5.uc.r.appspot.com/add-tutor',
+        payload
+      )
+    }
+
+    await fetchTutors()
+    closeCreateEditModal()
+  } catch (error) {
+    console.error('Error saving tutor:', error)
+  }
 }
 
-const closeCreateEditModal = () => (createEditTutorModal.value = false)
-const closeDeleteTutorModal = () => (deleteTutorModal.value = false)
-const saveTutor = () => {
-  console.log('Tutor saved:', tutor.value)
-  closeCreateEditModal()
+const deleteTutor = async () => {
+  try {
+    await axios.delete(
+      `https://dark-caldron-448714-u5.uc.r.appspot.com/delete-tutor/${selectedTutor.value._id}`
+    )
+    await fetchTutors()
+    closeDeleteTutorModal()
+  } catch (error) {
+    console.error('Error deleting tutor:', error)
+  }
 }
 
-const deleteTutor = () => {
-  console.log('Tutor deleted:', selectedTutor.value)
-  closeDeleteTutorModal()
+const closeCreateEditModal = () => {
+  createEditTutorModal.value = false
+}
+
+const closeDeleteTutorModal = () => {
+  deleteTutorModal.value = false
 }
 </script>
 
 <style scoped>
 .container {
   max-width: 1000px;
+}
+
+.opacity-50 {
+  opacity: 0.5;
+}
+
+.pointer-events-none {
+  pointer-events: none;
 }
 
 ::-webkit-scrollbar {
