cde4c9d7f44fa9468f267de78cd950a51b6ee380
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-10 11:03:46 +0000

    Adjust flashcards page layout for large screens

--- src/components/apps/flashcards/index.vue
+++ src/components/apps/flashcards/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-4 lg:flex-row">
+  <div class="flex flex-col gap-4 lg:h-screen">
     <!-- Home -->
     <div v-if="showHomeFlashcards" class="w-full">
       <div
@@ -40,7 +40,7 @@
     <!-- Form Container -->
     <div
       v-if="showCreateFlashcards"
-      class="mx-auto w-full rounded-xl bg-white p-8 shadow-lg dark:bg-[#111C44] dark:text-white"
+      class="mx-auto w-full rounded-xl bg-white p-8 dark:bg-[#111C44] dark:text-white"
     >
       <!-- Upload File -->
       <div
