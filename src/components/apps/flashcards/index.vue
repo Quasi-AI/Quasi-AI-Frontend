033ac3b4e668fe06efdb6ea8bac336ff12d259fd
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-14 21:35:16 +0000

    Update close

--- src/components/apps/flashcards/index.vue
+++ src/components/apps/flashcards/index.vue
@@ -242,6 +242,29 @@
       v-if="showCreateFlashcards"
       class="mx-auto w-full rounded-xl bg-white p-8 shadow-sm dark:bg-[#111C44] dark:text-white"
     >
+      <div class="flex justify-end pb-4">
+        <button
+          @click="closeFlashcardSetDetail"
+          class="text-gray-500 hover:text-red-500"
+          title="Close"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
+
       <!-- Upload File -->
       <div
         @click="triggerFileInput"
@@ -489,6 +512,8 @@ const closeFlashcardSetDetail = () => {
   selectedFlashcardSet.value = null
   showFlashcardSetDetail.value = false
   showHomeFlashcards.value = true
+  showCreateFlashcards.value = false
+  showPreviewFlashcards.value = false
 }
 
 // Format date
--- src/components/apps/questions/index.vue
+++ src/components/apps/questions/index.vue
@@ -202,6 +202,29 @@
       v-if="showCreateQuestions"
       class="mx-auto w-full rounded-xl bg-white p-8 shadow-sm dark:bg-[#111C44] dark:text-white"
     >
+      <div class="flex justify-end pb-4">
+        <button
+          @click="closeQuestionSetDetail"
+          class="text-gray-500 hover:text-red-500"
+          title="Close"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
+
       <!-- Upload File -->
       <div
         @click="triggerFileInput"
@@ -440,6 +463,8 @@ const closeQuestionSetDetail = () => {
   selectedQuestionSet.value = null
   showQuestionSetDetail.value = false
   showHomeQuestions.value = true
+  showCreateQuestions.value = false
+  showPreviewQuestions.value = false
 }
 
 // Format date
