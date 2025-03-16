9c48403c9592a2d8f09d6fc3ac098688bd1640cc
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-16 15:40:45 +0000

    Enhance profile image display in quizzes component by adding a default fallback

--- src/components/apps/quizzes/index.vue
+++ src/components/apps/quizzes/index.vue
@@ -86,9 +86,12 @@
             <!-- User details always at the bottom -->
             <div class="mt-auto flex items-center gap-3 pt-3">
               <img
-                :src="quiz.created_by?.profile"
+                :src="
+                  quiz.created_by?.profile ||
+                  'https://cdn-icons-png.flaticon.com/512/929/929422.png'
+                "
                 alt="Profile"
-                class="h-10 w-10 rounded-full object-cover"
+                class="h-10 w-10 rounded-full border object-cover dark:border-[#0C1438]"
               />
               <div>
                 <p class="font-semibold">{{ quiz.created_by?.name }}</p>
