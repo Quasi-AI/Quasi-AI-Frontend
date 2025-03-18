b629d353db2a1d2d1ad02042f01265b7e378f485
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-18 22:36:13 +0000

    rename button label

--- src/components/landing/index.vue
+++ src/components/landing/index.vue
@@ -340,7 +340,7 @@
             <button
               class="mt-4 w-full rounded-lg bg-[#5D3BEA] p-2 font-bold text-white"
             >
-              Sign up
+              Get started
             </button>
           </NuxtLink>
         </div>
