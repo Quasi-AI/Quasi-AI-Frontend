a6c8f5624807f04db7f6bea1588107495fdb5a9e
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-19 07:53:12 +0000

    Refactor sign-up link for better readability

--- src/components/landing/index.vue
+++ src/components/landing/index.vue
@@ -399,10 +399,7 @@
               integration
             </li>
           </ul>
-          <NuxtLink
-            v-if="!isLoggedIn"
-            to="/auth/sign-up"
-          >
+          <NuxtLink v-if="!isLoggedIn" to="/auth/sign-up">
             <button
               class="mt-4 w-full rounded-lg bg-[#5D3BEA] p-2 font-bold text-white"
             >
