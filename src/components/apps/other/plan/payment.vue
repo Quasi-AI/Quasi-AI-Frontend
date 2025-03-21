f4f7486231bfeb08616f5c8c4000cb443ee66265
danieladeabah <daniel.adeabah@amalitech.com>
2025-03-21 18:36:32 +0000

    Add payment component to handle price display in plan section

---
+++ src/components/apps/other/plan/payment.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Payment ({{ selectedPrice }})</div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute()
+const selectedPrice = ref(route.query.price || '0.00')
+</script>
