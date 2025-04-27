9ecbbfddf1dbc2821eb82e4f66cc9c69dc302f58
danieladeabah <daniel.adeabah@amalitech.com>
2025-04-27 21:29:33 +0000

    Update route title for apps page to improve clarity

--- src/layouts/app-wrapper.vue
+++ src/layouts/app-wrapper.vue
@@ -51,7 +51,7 @@ const greeting = computed(() => {
 // Define route-based titles
 const titles = {
   '/dashboard': greeting,
-  '/apps': 'Browse top apps',
+  '/apps': 'Apps',
   '/apps/questions': 'Questions',
   '/apps/flashcards': 'Smart Flashcards',
   '/apps/find-tutors': 'Tutors',
